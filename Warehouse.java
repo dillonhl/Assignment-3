80cfe0e1c2c8ddc30a10734dc834dcd8569f5a35
Ming-Hua Lawrence <mslawren@uci.edu>
2022-03-21 21:31:42 -0700

    Fill Warehouse.java
    I just filled Warehouse.java with its basic attributes and methods.

--- Warehouse.java
+++ Warehouse.java
@@ -1,3 +1,16 @@
 public class Warehouse {
-    
+	int stockNumber;
+	int warehouseNumber;
+	int productID;
+	String productName;
+	int warehouseQuantity;
+	int capacity;
+
+	void addQuantity(int warehouseNum, int productID, int quantity){
+
+	}
+
+	void decrQuantity(int warehouseNum, int productID, int quantity){
+
+	}
 }

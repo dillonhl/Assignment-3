5e41c5faf02aa15ade623dc86ec736bdc9066427
victormcastaneda <36462257+victormcastaneda@users.noreply.github.com>
2022-03-30 20:08:43 -0700

    Update Salesperson.java

--- Salesperson.java
+++ Salesperson.java
@@ -3,18 +3,18 @@ import java.io.FileReader;
 import java.util.*;
 
 public class Salesperson {
-    int salesperson_id;
-    String salesperson_name;
-    float commission_rate;
-    LinkedHashMap<Integer,Float> commissionsToPay;
-    float total_commissioned_earned;
-    float total_sales_made;
+	int salesperson_id;
+    	String salesperson_name;
+    	float commission_rate;
+    	LinkedHashMap<Integer,Float> commissionsToPay;
+    	float total_commission_earned;
+    	float total_sales_made;
 
-    Salesperson(int salesprsn_id){
-    	this.salesperson_id = salesprsn_id;
-    	String[] searched_salesprsn = null;
+    	Salesperson(int salesprsn_id){
+    		this.salesperson_id = salesprsn_id;
+    		String[] searched_salesprsn = null;
 		try {
-    		String line = ""; String splitBy = ",";
+    			String line = ""; String splitBy = ",";
 			BufferedReader br = new BufferedReader(new FileReader("Salesperson_List.csv"));
 			br.readLine();
 			//List<String> found_product = Collections.emptyList();
@@ -24,62 +24,65 @@ public class Salesperson {
 				if (Integer.parseInt(searched_salesprsn[0]) == salesprsn_id) {
 					this.salesperson_name = searched_salesprsn[1];
 					this.commission_rate = Float.parseFloat(searched_salesprsn[2]);
-					this.total_commissioned_earned = Float.parseFloat(searched_salesprsn[3]);
+					this.total_commission_earned = Float.parseFloat(searched_salesprsn[3]);
 					this.total_sales_made = Float.parseFloat(searched_salesprsn[4]);
 				}
 			}
 			br.close();
 			//return found_product;
-    	} catch (Exception e) {
+    		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		this.commissionsToPay = new LinkedHashMap<Integer,Float>();
-    }
+    	}
     
-    int getID(){
-        return salesperson_id;
-    }
+	int getID(){
+    		return salesperson_id;
+    	}
 
-    float getCommissionRate(){
-        return commission_rate;
-    }
+    	float getCommissionRate(){
+        	return commission_rate;
+    	}
 
-    void setCommissionRate(float commission)
-    {
-        commission_rate = commission;
-    }
+    	void setCommissionRate(float commission)
+    	{
+        	commission_rate = commission;
+    	}
 
-    void checkOffCommission(int invoice_id){
+    	void checkOffCommission(int invoice_id){
 
-    }
+    	}
+	
 /*
-    HashMap<int,float> getCommissionsToPay(){
+    	HashMap<int,float> getCommissionsToPay(){
 
-    }
+    	}
 */
-    float getCommissionEarned(){
-        return total_commissioned_earned;
-    }
+    	float getCommissionEarned(){
+        	return total_commission_earned;
+    	}
 
-    void addSalesMade(float sale){
-        total_sales_made += sale;
-    }
+    	void addSalesMade(float sale){
+        	total_sales_made += sale;
+    	}
 /*
-    float getTotalSalesMade() {
+    	float getTotalSalesMade() {
 
-    }
+    	}
 */
-    void addCommission(int invoice_id, float sale_commission){
+    	void addCommission(int invoice_id, float sale_commission){
     		this.commissionsToPay.put(invoice_id, sale_commission);
-    }
-    
-    @Override
-    public String toString() {
-    	return String.format("Salesperson [ID = %2d, Name = %20s, "
+		this.total_commission_earned += sale_commission;
+    	}
+	
+    	@Override
+    	public String toString() {
+    		return String.format("Salesperson [ID = %2d, Name = %20s, "
     			+ "Commission Rate = %3d%%, Total Commission Earned "
     			+ "= $%4.2f, Total Sales Made = $%4.2f]", this.salesperson_id,
     			this.salesperson_name, this.commission_rate,
-    			this.total_commissioned_earned, this.total_sales_made);
-    }
+    			this.total_commission_earned, this.total_sales_made);
+    	}
+	
 }
